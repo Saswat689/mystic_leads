7aca31c4e272eaec07d803ad1265c06e51e0cca9
saswat <saswatsingh629@gmail.com>
2024-05-05 14:13:29 +0530

    changed spotify login to discord

--- components/Auth/Signup.js
+++ components/Auth/Signup.js
@@ -146,7 +146,7 @@ const Signup = ({ providers, error }) => {
               </button>
 
               <button
-                onClick={() => signIn(providers.spotify.id)}
+                onClick={() => signIn(providers.discord.id)}
                 aria-label="signup with github"
                 className="text-body-color dark:text-body-color-dark dark:shadow-two mb-6 flex w-full items-center justify-center rounded-sm border border-stroke bg-[#f8f8f8] px-6 py-3 text-base outline-none transition-all duration-300 hover:border-primary hover:bg-primary/5 hover:text-primary dark:border-transparent dark:bg-[#2C303B] dark:hover:border-primary dark:hover:bg-primary/5 dark:hover:text-primary dark:hover:shadow-none"
               >
